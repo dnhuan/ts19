4368096806879fef7b84e3e3a78263d24c435e2a
dnhuan <huandngoc@gmail.com>
2019-06-06 03:10:57 +0700

    update socket.io for client side

--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 $(() => {
-    var socket = io();
+    var socket = io("http://bogddt.gq:8080");
     socket.on("realtime",(data) => {
         $('#current').text(data.o);
         $('#tong').text(data.s);
