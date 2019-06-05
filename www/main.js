9381209462038d2995237083ab3b7826ec89a8d6
dnhuan <huandngoc@gmail.com>
2019-06-06 03:50:14 +0700

    move from wss to https

--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 $(() => {
-    var socket = io("wss://bogddt.gq/socket",{transports: ['websocket', 'polling', 'flashsocket']});
+    var socket = io("https://bogddt.gq/socket",{transports: ['websocket', 'polling', 'flashsocket']});
     socket.on("realtime",(data) => {
         $('#current').text(data.o);
         $('#tong').text(data.s);
