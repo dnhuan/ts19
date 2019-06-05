7a6d0b71cd40d0d93e1cd5c1d36a5cd65e1e2edc
dnhuan <huandngoc@gmail.com>
2019-06-06 04:48:08 +0700

    added share buttons

--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 $(() => {
-    var socket = io("https://bogddt.gq:8080",{transports: ['websocket', 'polling', 'flashsocket']});
+    var socket = io("https://bogddt.gq:8083",{transports: ['websocket', 'polling', 'flashsocket']});
     socket.on("realtime",(data) => {
         $('#current').text(data.o);
         $('#tong').text(data.s);
