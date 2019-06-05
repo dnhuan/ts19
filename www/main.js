a3f8f5800ee01e2a65285cd75a8b85ea4dd981b5
dnhuan <huandngoc@gmail.com>
2019-06-06 03:37:50 +0700

    edit socket proxy

--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 $(() => {
-    var socket = io("http://bogddt.gq:8080",{transports: ['websocket', 'polling', 'flashsocket']});
+    var socket = io("http://bogddt.gq/socket",{transports: ['websocket', 'polling', 'flashsocket']});
     socket.on("realtime",(data) => {
         $('#current').text(data.o);
         $('#tong').text(data.s);
